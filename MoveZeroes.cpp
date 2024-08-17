91fb5071c72e0a20c3517614a76838e5ecb5b476
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-08-17 14:34:31 +0530

    Add files via upload

---
+++ MoveZeroes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                nums[count++] = nums[i];
+            }
+        }
+
+        while(count < nums.size()){
+            nums[count++] = 0;
+        }
+    }
+};
