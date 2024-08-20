f92f3181717dbd6913f78e52d4291892f66b697d
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-08-20 16:44:35 +0530

    Add files via upload

---
+++ FindDuplicates.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            int ind = abs(nums[i]) - 1;
+            nums[ind] = -nums[ind];
+            if(nums[ind] > 0){
+                ans.push_back(ind+1);
+            }
+        }
+        return ans;
+    }
+};
