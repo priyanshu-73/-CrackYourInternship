de64774eff602d92b84a672c28d0c47f6fd6c975
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-09-06 16:05:48 +0530

    Add files via upload

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            else
+            {
+                if (st.empty() || (s[i] == ')' and st.top() != '(') ||
+                    (s[i] == '}' and st.top() != '{') ||
+                    (s[i] == ']' and st.top() != '['))
+                    return false;
+                st.pop();
+            }
+        }
+        if (st.empty())
+            return true;
+        return false;
+    }
+};
