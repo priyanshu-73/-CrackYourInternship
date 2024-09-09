2ff1252b6d672fcdd08933ab8758915f28745fc1
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-09-09 12:17:53 +0530

    Update smallestWindowSubstring.cpp

--- smallestWindowSubstring.cpp
+++ smallestWindowSubstring.cpp
@@ -3,58 +3,40 @@ using namespace std;
 
 class Solution
 {
-public:
-    // Function to find the smallest window in the string s consisting
-    // of all the characters of string p.
-    string smallestWindow(string s, string p)
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    string smallestWindow (string s, string p)
     {
-        vector<int> mp(26, 0);
-        for (char ch : p)
-        {
-            mp[ch]++;
+        int count = 0;
+        vector<int> arr(26, 0);
+        for(int i = 0; i < p.size(); i++){
+            if(arr[p[i]-'a'] == 0)
+                count++;
+            arr[p[i]-'a']++;
         }
-
-        int count = mp.size(); // Count of unique characters to match
-        int min_len = INT_MAX;
-        int start = 0, i = 0, j = 0;
-
-        // Sliding window approach
-        while (j < s.length())
-        {
-            // Process the current character in the window
-            if (mp.find(s[j]) != mp.end())
-            {
-                mp[s[j]]--;
-                if (mp[s[j]] == 0)
-                {
-                    count--;
-                }
+        int len = INT_MAX;
+        int i = 0, j = 0;
+        string ans = "";
+        int start = -1;
+        while(j < s.length()){
+            arr[s[j]-'a']--;
+            if(arr[s[j]-'a'] == 0){
+                count--;
             }
-
-            // Shrink the window from the left if all characters are matched
-            while (count == 0)
-            {
-                if (j - i + 1 < min_len)
-                {
-                    min_len = j - i + 1;
-                    start = i; // Track the starting index of the smallest window
+            while(count == 0){
+                if(len > j - i + 1){
+                    len = j - i + 1;
+                    start = i;
                 }
-
-                // Shrink the window from the left by moving i forward
-                if (mp.find(s[i]) != mp.end())
-                {
-                    mp[s[i]]++;
-                    if (mp[s[i]] > 0)
-                    {
-                        count++;
-                    }
+                arr[s[i]-'a']++;
+                if(arr[s[i]-'a'] == 1){
+                    count++;
                 }
                 i++;
             }
-
             j++;
         }
-
-        return min_len != INT_MAX ? s.substr(start, min_len) : "-1";
+        return len != INT_MAX ? s.substr(start, len) : "-1";
     }
 };
